7361f77f8de71e337ff0d553178959392df89aac
Adriaan Boshoff <16230645+AdriaanBoshoff@users.noreply.github.com>
2023-08-01 08:38:04 +0200

    Add MenuBar, Nav Pane and Status Bar

--- RSMfmx_Client/ufrmMain.pas
+++ RSMfmx_Client/ufrmMain.pas
@@ -4,10 +4,16 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, udmStyles;
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, udmStyles,
+  FMX.Menus, FMX.StdCtrls, FMX.MultiView, FMX.Controls.Presentation;
 
 type
   TfrmMain = class(TForm)
+    mnbrMain: TMenuBar;
+    statMain: TStatusBar;
+    mltvwNav: TMultiView;
+    tlbHeader: TToolBar;
+    btnNavBurger: TSpeedButton;
   private
     { Private declarations }
   public
