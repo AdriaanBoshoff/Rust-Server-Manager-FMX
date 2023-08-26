9c128061c4a50d29cd143295f52a9bbb46f23db8
Adriaan Boshoff <16230645+AdriaanBoshoff@users.noreply.github.com>
2023-08-26 21:47:12 +0200

    Create uServerInfo.pas

---
+++ RSMfmx_Client/Units/uServerInfo.pas
@@ -0,0 +1,15 @@
+﻿unit uServerInfo;
+
+interface
+
+uses
+  RCON.Types;
+
+  // Globally Defined Variable that gets updated in RCON.Events
+var
+  serverInfoCurrent: TRCONServerInfo;
+
+implementation
+
+end.
+
