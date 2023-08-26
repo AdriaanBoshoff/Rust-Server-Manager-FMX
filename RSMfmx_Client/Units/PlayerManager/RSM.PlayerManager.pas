ce47e41e9c9a66d7f800e8471dde5bb152008c6f
Adriaan Boshoff <16230645+AdriaanBoshoff@users.noreply.github.com>
2023-08-26 23:45:03 +0200

    Added

---
+++ RSMfmx_Client/Units/PlayerManager/RSM.PlayerManager.pas
@@ -0,0 +1,59 @@
+﻿unit RSM.PlayerManager;
+
+interface
+
+uses
+  System.Generics.Collections, RCON.Types;
+
+type
+  TPlayerManager = class
+  public
+  { Public Variables }
+    onlinePlayers: TDictionary<string, TRCONPlayerListPlayer>;
+  public
+  { Public Methods }
+    constructor Create;
+    destructor Destroy; override;
+    procedure LoadOnlinePlayersFromArray(const PlayerList: TArray<TRCONPlayerListPlayer>);
+  end;
+
+var
+  playerManager: TPlayerManager;
+
+implementation
+
+{ TPlayerManager }
+
+constructor TPlayerManager.Create;
+begin
+  inherited;
+  onlinePlayers := TDictionary<string, TRCONPlayerListPlayer>.Create;
+end;
+
+destructor TPlayerManager.Destroy;
+begin
+  onlinePlayers.Free;
+  inherited;
+end;
+
+procedure TPlayerManager.LoadOnlinePlayersFromArray(const PlayerList: TArray<TRCONPlayerListPlayer>);
+begin
+  self.onlinePlayers.Clear;
+
+  for var aPlayer in PlayerList do
+    self.onlinePlayers.Add(aPlayer.SteamID, aPlayer);
+end;
+
+initialization
+begin
+  playerManager := TPlayerManager.Create;
+end;
+
+
+finalization
+begin
+  playerManager.Free;
+end;
+
+end.
+
