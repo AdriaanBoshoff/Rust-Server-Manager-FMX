ddc3afe3377af322a512337aed835d776e88b0f1
Adriaan Boshoff <16230645+AdriaanBoshoff@users.noreply.github.com>
2023-08-26 12:42:20 +0200

    Add and Handle onServerInfo rcon event

--- RSMfmx_Client/Units/RCON/RCON.Events.pas
+++ RSMfmx_Client/Units/RCON/RCON.Events.pas
@@ -3,7 +3,7 @@
 interface
 
 uses
-  RCON.Types;
+  RCON.Types, RCON.Commands;
 
 type
   TRCONEvents = class
@@ -19,6 +19,9 @@ var
 
 implementation
 
+uses
+  RCON.Parser, ufrmMain, System.SysUtils, System.DateUtils;
+
 { TRCONEvents }
 
 procedure TRCONEvents.OnRconMessage(const rconMessage: TRCONMessage);
@@ -26,11 +29,41 @@ begin
   // All messages received from rcon will be processed here.
 
   // ServerInfo
+  if rconMessage.aIdentifier = RCON_ID_SERVERINFO then
+    OnServerInfo(TRCONParser.ParseServerInfo(rconMessage.aMessage));
 end;
 
 procedure TRCONEvents.OnServerInfo(const serverInfo: TRCONServerInfo);
 begin
-//
+  // Players
+  frmMain.lblPlayerCountValue.Text := Format('%d / %d', [serverInfo.Players, serverInfo.MaxPlayers]);
+
+  // Queued
+  frmMain.lblQueuedValue.Text := serverInfo.Queued.ToString;
+
+  // Joining
+  frmMain.lblJoiningValue.Text := serverInfo.Joining.ToString;
+
+  // Network Out
+  frmMain.lblNetworkOutValue.Text := Format('%d b/s ↑', [serverInfo.NetworkOut]);
+
+  // Network In
+  frmMain.lblNetworkInValue.Text := Format('%d b/s ↓', [serverInfo.NetworkIn]);
+
+  // FPS
+  frmMain.lblServerFPSValue.Text := serverInfo.FPS.ToString;
+
+  // Entity Count
+  frmMain.lblServerEntityCountValue.Text := serverInfo.EntityCount.ToString;
+
+  // Protocol
+  frmMain.lblServerProtocolValue.Text := serverInfo.Protocol;
+
+  // Ram Usage
+  frmMain.lblServerMemoryUsageValue.Text := serverInfo.MemoryUsageSystem.ToString;
+
+  // Last Wipe
+  frmMain.lblLastWipeValue.Text := FormatDateTime('yyyy/mm/dd hh:nn:ss', serverInfo.SaveCreatedTime);
 end;
 
 initialization
