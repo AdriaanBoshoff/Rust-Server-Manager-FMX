8e837cc0549d2fbe41ae1e118643255751373b2a
Adriaan Boshoff <16230645+AdriaanBoshoff@users.noreply.github.com>
2023-09-01 19:23:59 +0200

    Load player avatar

--- RSMfmx_Client/Modules/PlayerManager/FrameItems/uframePlayerItem.pas
+++ RSMfmx_Client/Modules/PlayerManager/FrameItems/uframePlayerItem.pas
@@ -5,7 +5,8 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Graphics, FMX.Controls, FMX.Forms, FMX.Dialogs, FMX.StdCtrls,
-  FMX.Layouts, FMX.Objects, FMX.Controls.Presentation;
+  FMX.Layouts, FMX.Objects, FMX.Controls.Presentation, System.IOUtils,
+  System.Threading;
 
 type
   TframePlayerItem = class(TFrame)
@@ -31,33 +32,102 @@ type
     lblGameBansHeader: TLabel;
     lblGameBansValue: TLabel;
     procedure lblHealthResized(Sender: TObject);
-    procedure lblIPValueResized(Sender: TObject);
     procedure lblPingResized(Sender: TObject);
     procedure rctnglBGMouseEnter(Sender: TObject);
     procedure rctnglBGMouseLeave(Sender: TObject);
   private
     { Private declarations }
+    function GetAvatarIconFromXML(xml: string): string;
   public
     { Public declarations }
+    procedure LoadSteamAvatar(const steamID: string);
   end;
 
 implementation
 
+uses
+  Rest.Client, Rest.Types, Xml.XMLDoc, Xml.XMLIntf, ActiveX;
+
 {$R *.fmx}
+
+function TframePlayerItem.GetAvatarIconFromXML(xml: string): string;
+var
+  XMLDoc: IXMLDocument;
+  Node: IXMLNode;
+begin
+  Result := '';
+
+  XMLDoc := TXMLDocument.Create(nil);
+  try
+    XMLDoc.LoadFromXML(xml);
+
+    Node := XMLDoc.DocumentElement.ChildNodes.FindNode('avatarMedium');
+    if Assigned(Node) then
+      Result := Node.Text;
+  finally
+    XMLDoc := nil;
+  end;
+end;
 
 procedure TframePlayerItem.lblHealthResized(Sender: TObject);
 begin
   lytHealth.Width := lblHealth.Width;
 end;
 
-procedure TframePlayerItem.lblIPValueResized(Sender: TObject);
-begin
-  lytIPAddress.Width := lblIPHeader.Width + lblIPValue.Margins.Left + lblIPValue.Width;
-end;
-
 procedure TframePlayerItem.lblPingResized(Sender: TObject);
 begin
   lytPing.Width := lblPing.Width;
+end;
+
+procedure TframePlayerItem.LoadSteamAvatar(const steamID: string);
+begin
+  TTask.Run(
+    procedure
+    begin
+      var rest := TRESTRequest.Create(Self);
+      try
+        rest.Client := TRESTClient.Create(rest);
+        rest.Method := TRESTRequestMethod.rmGET;
+        rest.Response := TRESTResponse.Create(rest);
+        rest.Client.BaseURL := 'https://steamcommunity.com/profiles';
+        rest.Resource := steamID;
+        rest.Params.AddItem('xml', '1', TRESTRequestParameterKind.pkQUERY);
+        rest.Execute;
+
+        if (rest.Response.StatusCode = 200) and not rest.Response.Content.IsEmpty then
+        begin
+          var memStream := TMemoryStream.Create;
+          try
+            CoInitialize(nil);
+            var avatarURL := GetAvatarIconFromXML(rest.Response.Content);
+            CoUninitialize;
+
+            if not avatarURL.Trim.IsEmpty then
+            begin
+              TDownloadURL.DownloadRawBytes(avatarURL, memStream);
+
+              TThread.Synchronize(TThread.Current,
+                procedure
+                begin
+                  try
+                    crclAvatar.Fill.Bitmap.WrapMode := TWrapMode.TileStretch;
+                    crclAvatar.Fill.Bitmap.Bitmap.LoadFromStream(memStream);
+                  except
+                    on E: Exception do
+                    begin
+                      Exit;
+                    end;
+                  end;
+                end);
+            end;
+          finally
+            memStream.Free;
+          end;
+        end;
+      finally
+        rest.Free;
+      end;
+    end);
 end;
 
 procedure TframePlayerItem.rctnglBGMouseEnter(Sender: TObject);
