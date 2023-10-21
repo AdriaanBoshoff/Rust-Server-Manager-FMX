9a97eef501de7947ab44266d97cf46ad0a4583e7
Adriaan Boshoff <16230645+AdriaanBoshoff@users.noreply.github.com>
2023-10-21 21:18:34 +0200

    Plugin Installer Vendor Nav

--- RSMfmx_Client/Modules/PluginManager/PluginInstaller/ufrmPluginInstaller.pas
+++ RSMfmx_Client/Modules/PluginManager/PluginInstaller/ufrmPluginInstaller.pas
@@ -5,7 +5,8 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, udmStyles,
-  FMX.Layouts, FMX.Controls.Presentation, FMX.StdCtrls, FMX.Objects;
+  FMX.Layouts, FMX.Controls.Presentation, FMX.StdCtrls, FMX.Objects,
+  FMX.TabControl;
 
 type
   TfrmPluginInstaller = class(TForm)
@@ -17,6 +18,19 @@ type
     lblNavCodeFling: TLabel;
     rctnglNavLoneDesign: TRectangle;
     lblNavLoneDesign: TLabel;
+    tbcVendors: TTabControl;
+    tbtmuMod: TTabItem;
+    tbtmCodeFling: TTabItem;
+    tbtmLoneDesign: TTabItem;
+    procedure OnNavItemClick(Sender: TObject);
+    procedure OnNavItemMouseEnter(Sender: TObject);
+    procedure OnNavItemMouseLeave(Sender: TObject);
+  private
+    { Private Const }
+    const
+      UI_COLOR_DEFAULT = $FF1F222A;
+      UI_COLOR_HOVER = $FF6F0000;
+      UI_COLOR_SELECTED = $FF900000;
   private
     { Private declarations }
   public
@@ -30,4 +44,38 @@ implementation
 
 {$R *.fmx}
 
+procedure TfrmPluginInstaller.OnNavItemClick(Sender: TObject);
+begin
+  // Unselect other nav Items
+  for var aControl in grdpnllytBottomNav.Controls do
+  begin
+    if aControl is TRectangle then
+      TRectangle(aControl).Fill.Color := UI_COLOR_DEFAULT;
+  end;
+
+  // Change Nav Tab
+  if (Sender = rctnglNavuMod) then // Installed Plugins
+    tbcVendors.TabIndex := tbtmuMod.Index
+  else if (Sender = rctnglNavCodeFling) then // Plugin Installer
+    tbcVendors.TabIndex := tbtmCodeFling.Index
+  else if (Sender = rctnglNavLoneDesign) then // Plugin Updater
+    tbcVendors.TabIndex := tbtmLoneDesign.Index;
+
+  // Mark current nav Item as selected
+  (Sender as TRectangle).Fill.Color := UI_COLOR_SELECTED;
+end;
+
+procedure TfrmPluginInstaller.OnNavItemMouseEnter(Sender: TObject);
+begin
+  if (Sender as TRectangle).Fill.Color <> UI_COLOR_SELECTED then
+    (Sender as TRectangle).Fill.Color := UI_COLOR_HOVER;
+end;
+
+procedure TfrmPluginInstaller.OnNavItemMouseLeave(Sender: TObject);
+begin
+  if (Sender as TRectangle).Fill.Color <> UI_COLOR_SELECTED then
+    (Sender as TRectangle).Fill.Color := UI_COLOR_DEFAULT
+end;
+
 end.
+
