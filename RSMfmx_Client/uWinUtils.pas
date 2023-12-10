fc588a38eb494a34666a080ce0581b1fbf425044
Adriaan Boshoff <16230645+AdriaanBoshoff@users.noreply.github.com>
2023-12-11 00:57:00 +0200

    Add Overload for CreateProcess

--- RSMfmx_Client/uWinUtils.pas
+++ RSMfmx_Client/uWinUtils.pas
@@ -10,7 +10,9 @@ function CalculateMD5(const FileName: string): string;
 
 procedure OpenURL(const URL: string);
 
-function CreateProcess(const Exe, Params, AppTitle: string; const WaitUntilClosed: Boolean = False): Integer;
+function CreateProcess(const Exe, Params, AppTitle: string; const WaitUntilClosed: Boolean = False): Integer; overload;
+
+function CreateProcess(const Exe, Params, AppTitle: string; const WaitUntilClosed, InheritHandle: Boolean): Integer; overload;
 
 implementation
 
@@ -59,6 +61,45 @@ begin
 
   // Create the process
   if CreateProcessW(nil, PWideChar(ProgramPath), nil, nil, False, 0, nil, nil, StartupInfo, ProcessInfo) then
+  begin
+    // Wait for the process to complete (optional)
+    if WaitUntilClosed then
+      WaitForSingleObject(ProcessInfo.hProcess, INFINITE);
+
+    Result := ProcessInfo.dwProcessId;
+
+    // Close process and thread handles
+    CloseHandle(ProcessInfo.hProcess);
+    CloseHandle(ProcessInfo.hThread);
+  end
+  else
+  begin
+    // Failed to create the process
+    Result := -1;
+  end;
+end;
+
+function CreateProcess(const Exe, Params, AppTitle: string; const WaitUntilClosed, InheritHandle: Boolean): Integer;
+var
+  StartupInfo: TStartupInfoW;
+  ProcessInfo: TProcessInformation;
+  CommandLine: WideString;
+  ProgramPath: WideString;
+begin
+  // Return ProcessID. Default -1
+  Result := -1;
+
+  // Set up the command line and program path
+  CommandLine := Params;
+  ProgramPath := Exe + ' ' + CommandLine;
+
+  // Initialize the StartupInfo structure
+  FillChar(StartupInfo, SizeOf(StartupInfo), 0);
+  StartupInfo.lpTitle := PWideChar(AppTitle);
+  StartupInfo.cb := SizeOf(StartupInfo);
+
+  // Create the process
+  if CreateProcessW(nil, PWideChar(ProgramPath), nil, nil, InheritHandle, 0, nil, nil, StartupInfo, ProcessInfo) then
   begin
     // Wait for the process to complete (optional)
     if WaitUntilClosed then
