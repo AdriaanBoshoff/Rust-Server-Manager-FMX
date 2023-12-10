1f2cd30dce395b41603c4fe347449af9264c42a6
Adriaan Boshoff <16230645+AdriaanBoshoff@users.noreply.github.com>
2023-12-10 23:35:50 +0200

    RSM.Core.GetRSMCache{Dir/Path}

--- RSMfmx_Client/Units/RSM/RSM.Core.pas
+++ RSMfmx_Client/Units/RSM/RSM.Core.pas
@@ -12,12 +12,18 @@ type
       { TRSMCorePaths Private Const }
         const
           FOLDER_RSM = 'RSM';
+          FOLDER_Cache = 'Cache';
       public
       { TRSMCorePaths Public Class Functions }
+        { Program Root }
         class function GetRootDir: string;
         class function GetRootPath: string;
+        { RSM Data Folder }
         class function GetRSMDataDir: string;
         class function GetRSMDataPath: string;
+        { RSM Cache Folder }
+        class function GetRSMCacheDir: string;
+        class function GetRSMCachePath: string;
       end;
   public
   { TRSMCore Public Variables }
@@ -34,27 +40,49 @@ var
 implementation
 
 uses
-  System.SysUtils;
+  System.SysUtils, System.IOUtils;
 
 { TRSMCore.TRSMCorePaths }
 
 class function TRSMCore.TRSMCorePaths.GetRootDir: string;
 begin
+  // Get Program Root Dir
   Result := ExtractFileDir(ParamStr(0));
 end;
 
 class function TRSMCore.TRSMCorePaths.GetRootPath: string;
 begin
+ // Get Program Root Path
   Result := ExtractFilePath(ParamStr(0));
+end;
+
+class function TRSMCore.TRSMCorePaths.GetRSMCacheDir: string;
+begin
+  Result := TPath.Combine(Self.GetRSMDataDir, FOLDER_Cache) + TPath.DirectorySeparatorChar;
+
+  // Remove dir separator if exists
+  if Result[High(Result)] = TPath.DirectorySeparatorChar then
+    Delete(Result, High(Result), 1);
+end;
+
+class function TRSMCore.TRSMCorePaths.GetRSMCachePath: string;
+begin
+  Result := TPath.Combine(Self.GetRSMDataDir, FOLDER_Cache);
+
+  // Add Dir separator if not exists
+  if Result[High(Result)] <> TPath.DirectorySeparatorChar then
+    Result := Result + TPath.DirectorySeparatorChar;
 end;
 
 class function TRSMCore.TRSMCorePaths.GetRSMDataDir: string;
 begin
+  // Get Program Data Folder dir
   Result := Self.GetRootPath + FOLDER_RSM;
 end;
 
 class function TRSMCore.TRSMCorePaths.GetRSMDataPath: string;
 begin
+  // Get Program Data Folder Path
   Result := Self.GetRootPath + FOLDER_RSM + '\';
 end;
 
@@ -64,11 +92,13 @@ constructor TRSMCore.Create;
 begin
   inherited;
 
+  // Create Paths
   Self.Paths := TRSMCorePaths.Create;
 end;
 
 destructor TRSMCore.Destroy;
 begin
+  // Free Paths
   Self.Paths.Free;
 
   inherited;
@@ -76,12 +106,14 @@ end;
 
 initialization
 begin
+  // Create rsmCore variable when unit is loaded
   rsmCore := TRSMCore.Create;
 end;
 
 
 finalization
 begin
+  // Free rsmCore when unit is released
   rsmCore.Free;
 end;
 
