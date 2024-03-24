d0cc527093f3bda04980b4a208a1491e181a0242
Adriaan Boshoff <16230645+AdriaanBoshoff@users.noreply.github.com>
2024-03-24 12:31:27 +0200

    Fixed bug that causes CPU affinity selection to crash

--- RSMfmx_Client/Forms/Affinity Selector/ufrmAffinitySelect.pas
+++ RSMfmx_Client/Forms/Affinity Selector/ufrmAffinitySelect.pas
@@ -6,7 +6,8 @@ uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Objects,
   FMX.Controls.Presentation, FMX.StdCtrls, ufrmMain, FMX.Layouts, FMX.ListBox,
-  System.Skia, FMX.Skia, udmStyles;
+  System.Skia, FMX.Skia, udmStyles, FMX.ListView.Types, FMX.ListView.Appearances,
+  FMX.ListView.Adapters.Base, FMX.ListView;
 
 type
   TAppender<T> = class
@@ -20,8 +21,9 @@ type
     pnlFooter: TPanel;
     btnOk: TButton;
     btnCancel: TButton;
-    lstAffinity: TListBox;
     lblInfo: TLabel;
+    vrtscrlbxAffinity: TVertScrollBox;
+    lytCheckboxContainer: TLayout;
     procedure FormCreate(Sender: TObject);
     procedure btnCancelClick(Sender: TObject);
     procedure btnOkClick(Sender: TObject);
@@ -53,13 +55,6 @@ end;
 
 procedure TfrmSelectAffinity.GetSavedAffinity;
 begin
-  var arrAffinity := serverConfig.ServerAffinity;
-
-  for var I := Low(arrAffinity) to High(arrAffinity) do
-  begin
-    if lstAffinity.Items.Count >= I then
-      lstAffinity.ListItems[arrAffinity[I]].IsChecked := True;
-  end;
 end;
 
 procedure TfrmSelectAffinity.btnCancelClick(Sender: TObject);
@@ -71,12 +66,21 @@ procedure TfrmSelectAffinity.btnOkClick(Sender: TObject);
 begin
   var arrSelected: TArray<Integer>;
 
-  for var I := 0 to lstAffinity.Count - 1 do
+  for var control in lytCheckboxContainer.Controls do
   begin
-    if lstAffinity.ListItems[I].IsChecked then
-      TAppender<Integer>.Append(arrSelected, I);
+    if not (control is TCheckBox) then
+      Continue;
+
+    if (control as TCheckBox).IsChecked then
+      TAppender<Integer>.Append(arrSelected, control.Tag);
   end;
 
+//  for var I := 0 to lstAffinity.Count - 1 do
+//  begin
+//    if lstAffinity.ListItems[I].IsChecked then
+//      TAppender<Integer>.Append(arrSelected, I);
+//  end;
+//
   serverConfig.ServerAffinity := arrSelected;
   serverConfig.SaveConfig;
 
@@ -90,20 +94,41 @@ end;
 
 procedure TfrmSelectAffinity.PopulateCPUs;
 begin
-  lstAffinity.BeginUpdate;
+  lytCheckboxContainer.BeginUpdate;
   try
     for var I := 0 to GetNumberOfProcessors - 1 do
     begin
-      var aItem := TListBoxItem.Create(lstAffinity);
+      var aItem := TCheckBox.Create(lytCheckboxContainer);
+      aItem.Parent := lytCheckboxContainer;
+      aItem.Align := TAlignLayout.Bottom;
+      aItem.Name := 'chkCPU' + I.ToString;
       aItem.Text := 'CPU ' + I.ToString;
-      aItem.Selectable := False;
+      aItem.Tag := I;
       aItem.CanFocus := False;
+      aItem.Margins.Top := 3;
 
-      lstAffinity.AddObject(aItem);
+      for var selectedCPU in serverConfig.ServerAffinity do
+      begin
+        if I = selectedCPU then
+          aItem.IsChecked := True;
+      end;
     end;
   finally
-    lstAffinity.EndUpdate;
+    lytCheckboxContainer.EndUpdate;
   end;
+
+  // Resize Scrollbox
+  var totalHeight := 0.0;
+  for var control in lytCheckboxContainer.Controls do
+  begin
+    if control is TCheckBox then
+    begin
+      totalHeight := totalHeight + control.Height;
+      totalHeight := totalHeight + control.Margins.Top;
+    end;
+  end;
+
+  lytCheckboxContainer.Height := totalHeight;
 end;
 
 { TAppender<T> }
