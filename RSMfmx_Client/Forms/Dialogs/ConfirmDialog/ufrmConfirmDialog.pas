ee0fe7210c846ccaf85df32bab30ef8365fb785b
Adriaan Boshoff <16230645+AdriaanBoshoff@users.noreply.github.com>
2024-10-08 15:08:56 +0200

    Added

---
+++ RSMfmx_Client/Forms/Dialogs/ConfirmDialog/ufrmConfirmDialog.pas
@@ -0,0 +1,26 @@
+unit ufrmConfirmDialog;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+
+type
+  TfrmConfirmDialog = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmConfirmDialog: TfrmConfirmDialog;
+
+  // TODO: Custom Confirm Dialogs
+
+implementation
+
+{$R *.fmx}
+
+end.
